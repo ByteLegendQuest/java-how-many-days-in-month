43d8170867c6f04247a2d108286ae662d9b1b1f2
gtn1024 <tony.bazhang@gmail.com>
2022-04-04 13:27:29 +0000

    Update at 2022-04-04T13:27:29.695567Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -18,6 +18,28 @@ public class Challenge {
      * <p>`howManyDaysInMonth(1900, 2)` returns 28.
      */
     public static int howManyDaysInMonth(int year, int month) {
-        return 0;
+        boolean leap = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+        int day = 0;
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                day = 31;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                day = 30;
+                break;
+            case 2:
+                day = leap ? 29 : 28;
+                break;
+        }
+        return day;
     }
 }
