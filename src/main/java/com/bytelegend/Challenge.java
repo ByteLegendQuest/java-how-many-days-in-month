89e8caf75eb31ad84ae2b38d08942ac6412274ce
saveunhappy <925147954@qq.com>
2022-05-08 09:11:29 +0000

    Update at 2022-05-08T09:11:29.764474Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -18,6 +18,36 @@ public class Challenge {
      * <p>`howManyDaysInMonth(1900, 2)` returns 28.
      */
     public static int howManyDaysInMonth(int year, int month) {
-        return 0;
+        int days = 0;
+        boolean isLeapYear = (year % 100 == 0 && year % 400 == 0) || (year % 100 != 0 && year % 4 == 0);
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                days = 31;
+                break;
+            case 2:
+                if (isLeapYear) {
+                    days = 29;
+                } else {
+                    days = 28;
+                }
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                days = 30;
+                break;
+            default:
+                System.out.println("error!!!");
+                break;
+        }
+        return days;
+    }
     }
 }
