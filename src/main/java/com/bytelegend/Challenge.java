a39dc71615106d210c85db66e7c71c346ed5a07c
SpaceTimeAdministrator <106767177+SpaceTimeAdministrator@users.noreply.github.com>
2022-06-13 09:16:40 +0000

    Update at 2022-06-13T09:16:40.778667Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -33,7 +33,7 @@ public class Challenge {
             case 11:
                 return 30;
             case 2:
-                if ((year % 400 == 0) || (year % 100 != 0) && (year % 4 == 0)) {
+                 if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
                     return 29;
                 } else {
                     return 28;
