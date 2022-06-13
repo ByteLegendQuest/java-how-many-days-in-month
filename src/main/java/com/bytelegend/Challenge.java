272437bdfd2ff66095b4a7e6a819f8f42c54176a
SpaceTimeAdministrator <106767177+SpaceTimeAdministrator@users.noreply.github.com>
2022-06-13 09:15:22 +0000

    Update at 2022-06-13T09:15:22.083838Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -18,50 +18,27 @@ public class Challenge {
      * <p>`howManyDaysInMonth(1900, 2)` returns 28.
      */
     public static int howManyDaysInMonth(int year, int month) {
-        int a = 0;
         switch (month) {
             case 1:
-                a = 31;
-                break;
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                return 31;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                return 30;
             case 2:
                 if ((year % 400 == 0) || (year % 100 != 0) && (year % 4 == 0)) {
-                    a = 29;
-                    break;
+                    return 29;
                 } else {
-                    a = 28;
-                    break;
+                    return 28;
                 }
-            case 3:
-                a = 31;
-                break;
-            case 4:
-                a = 30;
-                break;
-            case 5:
-                a = 31;
-                break;
-            case 6:
-                a = 30;
-                break;
-            case 7:
-                a = 31;
-                break;
-            case 8:
-                a = 31;
-                break;
-            case 9:
-                a = 30;
-                break;
-            case 10:
-                a = 31;
-                break;
-            case 11:
-                a = 30;
-                break;
-            case 12:
-                a = 31;
-                break;
         }
-        return a;
+        return 0;
     }
 }
