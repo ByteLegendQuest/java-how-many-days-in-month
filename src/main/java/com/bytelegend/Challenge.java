2adba18bf40e2edb7c6f99ca26dbce574a10d10d
Yeely0162 <Yeely01621@163.com>
2022-06-28 14:57:31 +0000

    Update at 2022-06-28T14:57:31.630437Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -18,6 +18,33 @@ public class Challenge {
      * <p>`howManyDaysInMonth(1900, 2)` returns 28.
      */
     public static int howManyDaysInMonth(int year, int month) {
-        return 0;
+        boolean flag = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+        return selectMonth(month, flag);
+    }
+
+    public static int selectMonth(int month, boolean a) {
+        int day = 0;
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                day = 31;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                day = 30;
+                break;
+            case 2:
+                if (a) day = 29;
+                else day = 28;
+                break;
+        }
+        return day;
     }
 }
