c89991461e4c5891206462e456e957946904a1c3
shiguoqing99 <3118482822@qq.com>
2022-09-17 11:46:22 +0000

    Update at 2022-09-17T11:46:22.888641Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -32,7 +32,7 @@ public class Challenge {
             case 9:
             case 11:
                 return 30;
-            case 2:
+            default:
                 if (year % 100 == 0) {
                     if (year % 400 == 0) {
                         return 29;
@@ -41,8 +41,9 @@ public class Challenge {
                     }
                 } else if (year % 4 == 0) {
                     return 29;
+                } else {
+                    return 28;
                 }
         }
-        return 0;
     }
 }
