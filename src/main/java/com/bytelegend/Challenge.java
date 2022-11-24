4f6214f4933683a7677b42084658f3fa39ff4cf5
Tomcat-Guo <zhiqiang.guo@shopee.com>
2022-11-24 10:51:26 +0000

    Update at 2022-11-24T10:51:26.077290Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -19,18 +19,23 @@ public class Challenge {
      */
     public static int howManyDaysInMonth(int year, int month) {
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-            return switch (month) {
-                case 1, 3, 5, 7, 8, 10, 12 -> 31;
-                case 2 -> 29;
-                case 4, 6, 9, 11 -> 30;
-                default -> 0;
-            };
+            switch (month) {
+                case 1, 3, 5, 7, 8, 10, 12:
+                    return 31;
+                case 2:
+                    return 29;
+                case 4, 6, 9, 11:
+                    return 30;
+            }
         }
-        return switch (month) {
-            case 1, 3, 5, 7, 8, 10, 12 -> 31;
-            case 2 -> 28;
-            case 4, 6, 9, 11 -> 30;
-            default -> 0;
-        };
+        switch (month) {
+            case 1, 3, 5, 7, 8, 10, 12:
+                return 31;
+            case 2:
+                return 28;
+            case 4, 6, 9, 11:
+                return 30;
+        }
+        return 0;
     }
 }
