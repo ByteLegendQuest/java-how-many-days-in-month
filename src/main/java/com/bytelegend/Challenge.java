b7f13661e236c249eae6c4dccdae430c44d36721
DasBrain <johannes13@johannes13.de>
2023-05-20 18:11:05 +0000

    Update at 2023-05-20T18:11:05.540591Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -18,6 +18,17 @@ public class Challenge {
      * <p>`howManyDaysInMonth(1900, 2)` returns 28.
      */
     public static int howManyDaysInMonth(int year, int month) {
-        return 0;
+        return switch (month) {
+            case 1, 3, 5, 7, 8, 10, 12 -> 31;
+            case 4, 6, 9, 11 -> 30;
+            default -> feb(year);
+        };
+    }
+
+    private static int feb(int year) {
+        if (year % 400 == 0) return 29;
+        if (year % 100 == 0) return 28;
+        if (year % 4 == 0) return 29;
+        return 28;
     }
 }
