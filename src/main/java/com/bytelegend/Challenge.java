c26a2ca609cf146e8c4a04511b751d1ec53c615c
GL <ligan@oregonstate.edu>
2023-07-08 15:04:50 -0700

    Update Challenge.java

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -17,7 +17,13 @@ public class Challenge {
      *
      * <p>`howManyDaysInMonth(1900, 2)` returns 28.
      */
+    public static boolean isLeapYear(int year) {
+        return (year % 400 == 0) || ((year % 4 ==0) && (year % 100 != 0));
+    }
+    
     public static int howManyDaysInMonth(int year, int month) {
-        return 0;
+        int[] daysInMonth = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if (isLeapYear(year) && month == 2) return 29;
+        return daysInMonth(month - 1);
     }
 }
